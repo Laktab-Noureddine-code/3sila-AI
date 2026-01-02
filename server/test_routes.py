e8cbf5da6c31e102c05aa0f632c870d7e23cdf84
Noureddine Laktab <noureddine.laktab15@gmail.com>
2026-01-02 11:07:49 +0100

    adding inventory.ini and configure jenkins

--- server/test_routes.py
+++
@@ -1,6 +0,0 @@
-from app.main import app
-
-for route in app.routes:
-    if hasattr(route, 'path'):
-        methods = getattr(route, 'methods', None)
-        print(f"Path: {route.path}, Methods: {methods}")
